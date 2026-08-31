aa67bf3d90f60c9a7c990c4012ab11339b3212ec
Too-Zestyy <violet.steers.dev@gmail.com>
2026-08-31 16:34:04 +0100

    feat(GlickoCalc.Test): add test case using example from glicko 2 paper

--- GlickoCalc.Tests/UnitTestSteps.fs
+++ GlickoCalc.Tests/UnitTestSteps.fs
@@ -39,14 +39,31 @@ let TestTwoEqualPlayersHaveRatingUpdatedAsExpected () =
         p2Rating, p2Deviation, p2Volatility, 
         [|p1Rating|], [|p1Deviation|], [|Constants.Loss|], 
         Constants.DefaultSystemConstant, Constants.DefaultConvergenceTolerance)
-    
-    Assert.That(System.Math.Round(Convert.ToGlickoOneRating p1NRating, 2), Is.EqualTo 1662.31, "The winning player did not have their rating updated as expected.")
-    Assert.That(System.Math.Round(Convert.ToGlickoOneRating p2NRating, 2), Is.EqualTo 1337.69, "The losing player did not have their rating updated as expected.")
 
-    Assert.That(round (Convert.ToGlickoOneDeviation p1NDeviation), Is.EqualTo 290, "The winning player did not have their deviation updated as expected.")
-    Assert.That(round (Convert.ToGlickoOneDeviation p2NDeviation), Is.EqualTo 290, "The losing player did not have their deviation updated as expected.")
+    Assert.Multiple( fun () ->
+        Assert.That(System.Math.Round(Convert.ToGlickoOneRating p2NRating, 2), Is.EqualTo 1337.69, "The losing player did not have their rating updated as expected.")
+        Assert.That(round (Convert.ToGlickoOneDeviation p2NDeviation), Is.EqualTo 290, "The losing player did not have their deviation updated as expected.")
+        Assert.That(p2Volatility, Is.EqualTo 0.05999968, "The losing player did not have their volatility updated as expected.")
+    )
     
-    Assert.That(p1Volatility, Is.EqualTo 0.05999968, "The winning player did not have their volatility updated as expected.")
-    Assert.That(p2Volatility, Is.EqualTo 0.05999968, "The losing player did not have their volatility updated as expected.")
 
 // TODO: Add example from glicko 2 paper
+[<Test>]
+let TestImplementationMatchesPaperExample () = 
+    // While some of the figures used in this test case already exist within `Constants`, all numbers have been hardcoded to maintain consistency with the paper if 
+    // the constants are ever changed.
+
+    let p1Rating, p1Deviation, p1Volatility = Convert.ToGlickoTwoRating 1500.0, Convert.ToGlickoTwoDeviation 200.0, 0.06
+
+    let p1NRating, p1NDeviation, p1NVolatility = Steps.UpdatePlayerFromMatches(
+        p1Rating, p1Deviation, p1Volatility, 
+        [|Convert.ToGlickoTwoRating 1400.0; Convert.ToGlickoTwoRating 1550.0; Convert.ToGlickoTwoRating 1700.0|], 
+        [|Convert.ToGlickoTwoDeviation 30; Convert.ToGlickoTwoDeviation 100; Convert.ToGlickoTwoDeviation 300|], 
+        [|Constants.Win; Constants.Loss; Constants.Loss|], 
+        0.5, 0.000001)
+
+    Assert.Multiple( fun () ->
+        Assert.That(System.Math.Round(p1NRating, 4), Is.EqualTo -0.2069, "The player's rating does not match the paper's calculations")
+        Assert.That(System.Math.Round(p1NDeviation, 4), Is.EqualTo 0.8722, "The player's deviation does not match the paper's calculations")
+        Assert.That(System.Math.Round(p1NVolatility, 5), Is.EqualTo 0.05999, "The player's volatility does not match the paper's calculations")
+    )
